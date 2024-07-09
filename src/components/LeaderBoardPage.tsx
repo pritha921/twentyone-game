98c5e749e4d0763df36f9b340f045e4cf8dc6b74
Pritha Sen <prithasen006@gmail.com>
2024-07-09 10:49:27 +0530

    formatted the code.

--- src/components/LeaderBoardPage.tsx
+++ src/components/LeaderBoardPage.tsx
@@ -1,42 +1,3 @@
-// import { useLocation, useNavigate } from "react-router-dom";
-// import styles from "./LeaderboardPage.module.css";
-
-// interface User {
-//   name: string;
-//   status: string;
-// }
-
-// const LeaderboardPage = () => {
-//   const location = useLocation();
-//   const navigate = useNavigate();
-//   const { users } = location.state as { users: User[] };
-
-//   return (
-//     <div className={styles.leaderboardContainer}>
-//       <h1>Leaderboard</h1>
-//       <div className={styles.listContainer}>
-//         <div className={styles.listHeader}>
-//           <div className={styles.column}>Players</div>
-//           <div className={styles.column}>Status</div>
-//         </div>
-//         {users.map((user, index) => (
-//           <div key={index} className={styles.listItem}>
-//             <div className={styles.column}>{user.name}</div>
-//             <div className={styles.column}>{user.status}</div>
-//           </div>
-//         ))}
-//       </div>
-//       <button onClick={() => navigate("/")} className={styles.backToHome}>
-//         Back to Home
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default LeaderboardPage;
-
-
-
 import { useNavigate } from "react-router-dom";
 import { useGame } from "../models/GameContext";
 import styles from "./LeaderboardPage.module.css";
@@ -68,5 +29,3 @@ const LeaderboardPage = () => {
 };
 
 export default LeaderboardPage;
-
-
