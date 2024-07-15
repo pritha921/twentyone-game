05e32ddb010809cb89820dd750993ed6c7297242
Pritha <prithasen006@gmail.com>
2024-07-15 17:38:57 +0530

    Added link  in the heading.

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,17 +1,22 @@
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import './HeadingStyle.css'
+import { Link } from 'react-router-dom';
+import './HeadingStyle.css';
 
 export default function NavBar() {
   return (
-    <Box sx={{ flexGrow: 1}}>
-      <AppBar position="static"  sx={{ backgroundColor: '#ffff80' }}>
-        <Toolbar variant="dense"
-        sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-       <h1 className='heading-content'>
-        BAGRAM GAME!!
-       </h1>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{ backgroundColor: '#ffff80' }}>
+        <Toolbar
+          variant="dense"
+          sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+        >
+          <Link to="/" className="heading-link">
+            <h1 className="heading-content">
+              BAGRAM GAME!!
+            </h1>
+          </Link>
         </Toolbar>
       </AppBar>
     </Box>
